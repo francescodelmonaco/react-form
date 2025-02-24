cb6bfba773ff1e5e87a013b6b82825ac50cd911e
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-24 15:56:51 +0100

    add: form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,21 @@
-const Form = () => {
-    return (
-        <form action="">
+import { useState } from "react";
 
+const Form = () => {
+    const [title, setTitle] = useState("Titolo dell'articolo")
+
+    return (
+        <form action="" className="margin-60">
+            <h2>Form per l'inserimento di un nuovo articolo</h2>
+
+            <div className="input-flex">
+                <input
+                    type="text"
+                    value={title}
+                    onChange={e => { setTitle(e.target.value) }}
+                />
+
+                <button>Aggiungi</button>
+            </div>
         </form>
     )
 };
