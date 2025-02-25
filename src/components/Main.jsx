3d0593f5de941bcf2d7193d8146da07401b88c07
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-25 10:00:17 +0100

    add: reset setTitle input

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,6 +14,7 @@ const Main = () => {
     const addArticle = (e) => {
         e.preventDefault();
         setArticles([...articles, title]);
+        setTitle("");
     };
 
     // funzione per eliminare articolo nella lista
